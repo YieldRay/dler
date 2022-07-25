525424c34c0d584a919598ab030f2eae8a229f71
YieldRay <565332016@qq.com>
2022-07-25 12:07:56 +0800

    0.2.5

--- cli.js
+++ cli.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 const download = require('./lib/dler.js');
+const print = process.stdout.write.bind(process.stdout);
+
 const args = process.argv.slice(2);
 if (args.length < 1) {
     console.log('Usage: dler <url> <saveAs>');
@@ -8,23 +10,33 @@ if (args.length < 1) {
 
 const url = args[0];
 const saveAs = args[args.length - 1] === url ? '' : args[args.length - 1];
+const printToStartOfLine = (s => {
+    let lastLineCount = 0;
+    return s => {
+        const str = String(s);
+        print(' '.repeat(lastLineCount) + '\r' + str);
+        lastLineCount = str.length;
+    };
+})();
 
 if (new RegExp('^https?://').test(url)) {
     download(url, {
         filePath: saveAs,
         onProgress: (received, total) => {
             if (total === 0) {
-                console.log(`[received ${received} bytes] unknown%`);
+                printToStartOfLine(`[received ${received} bytes] unknown%`);
             } else {
                 const percentage = received / total;
                 const scale = 30;
                 const blocks = Math.round(percentage * scale);
                 const spaces = scale - blocks;
                 const bar = '█'.repeat(blocks) + ' '.repeat(spaces);
-                console.log(`[${bar}] ${Math.floor(percentage * 100)}%`);
+                printToStartOfLine(`[${bar}] ${Math.floor(percentage * 100)}%`);
             }
         },
-    }).then(path => console.log(`Downloaded to ${path}`));
+    })
+        .then(path => console.log(`\nDownloaded to ${path}`))
+        .catch(console.error);
 } else {
     console.log('URL is not valid');
 }
